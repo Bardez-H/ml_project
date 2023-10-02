5b7989e30bc57a6ca57a9cf8c70fc0b5309909d3
Bardez-H <bardehitesh@gmail.com>
2023-10-02 21:44:07 +0530

    MOdel Trainer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import Data_TransFormation_Config, Data_Transformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
 
 if __name__ == '__main__':
@@ -16,7 +17,12 @@ try:
     
     # data_transformation_config = Data_TransFormation_Config()
     data_transformation = Data_Transformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+    # Model Training
+    model_tariner = ModelTrainer()
+    print(model_tariner.initiate_model_trainer(train_arr, test_arr))
+
 
 except Exception as e:
     logging.info("Custom Exception")
